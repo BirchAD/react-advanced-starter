db14a65ebc671cc14060c4bb754e3276f5f457d8
BirchAD <andrewbirch1882@icloud.com>
2023-04-18 15:38:13 +0100

    usestate-array done

--- app/src/01-useState/useState-array.jsx
+++ app/src/01-useState/useState-array.jsx
@@ -1,6 +1,37 @@
+import data from '../../../data'
+import { useState } from 'react' 
+
 const UseStateArray = () => {
-    return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data)
+
+  const removePerson = (id) => {
+    setPeople(people.filter(person => person.id !== id))
+  };
+
+  const clearPeople = () => {
+    setPeople([]);
   };
   
-  export default UseStateArray;
-  
+  return (
+    <>
+      <h2>useState array example</h2>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button className="btn" onClick={() => removePerson(id)}>
+              Remove Me
+            </button>
+          </div>
+        );
+      })}
+
+      <div>
+        <button onClick={clearPeople}>Clear List</button>
+      </div>
+    </>
+  );
+};
+
+export default UseStateArray;

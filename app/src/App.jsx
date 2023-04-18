26a299e21302840976dd0e6d8edfbdb142a6b234
BirchAD <andrewbirch1882@icloud.com>
2023-04-18 15:55:08 +0100

    usestats done

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,11 +2,11 @@ import './App.css'
 import ErrorExample from './01-useState/error-example'
 import UseStateBasics from "./01-useState/useState-basics";
 import UseStateArray from './01-useState/useState-array';
-
+import UseStateObject from './01-useState/useState-object.jsx';
 function App() {
   return (
     <div className="container">
-      < UseStateArray />
+      < UseStateObject />
     </div>
   )
 }

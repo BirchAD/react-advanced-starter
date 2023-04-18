69704cc58ab4236e86d81cd6dfe37ba5e6d56fb0
BirchAD <andrewbirch1882@icloud.com>
2023-04-18 11:31:46 +0100

    first exercise

--- app/src/01-useState/error-example.jsx
+++ app/src/01-useState/error-example.jsx
@@ -1,5 +1,22 @@
+import { useState } from 'react';
+
+let count = 0
+
 const ErrorExample = () => {
-    return <h2>useState error example</h2>;
+  
+  const [count, setCount] = useState(0);
+
+   function handleClick() {
+    setCount(count + 1)
+   } 
+
+    return (
+      <>
+        <h2>useState error example</h2>
+        <h2>{count}</h2>
+        <button onClick={handleClick}>Click</button>
+      </>
+    );
   };
   
   export default ErrorExample;
--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import ErrorExample from './01-useState/error-example'
+import UseStateBasics from "./01-useState/useState-basics";
 
 function App() {
   return (
     <div className="container">
-     
+      < UseStateBasics />
     </div>
   )
 }

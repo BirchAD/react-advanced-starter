bad1c805996372012702f13b11e15267db4f7b3a
BirchAD <andrewbirch1882@icloud.com>
2023-04-19 15:23:51 +0100

    useeffect exercises done

--- app/src/02-useEffect/useEffect-fetch-data.jsx
+++ app/src/02-useEffect/useEffect-fetch-data.jsx
@@ -4,7 +4,7 @@ const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
   // TODO 1: set up 'users, setUsers' state values
-
+    const [users, setUsers] = useState([])
   /**
    * TODO 2: Set up asynchronous function getUsers()
    *
@@ -12,19 +12,42 @@ const UseEffectFetchData = () => {
    * await res.json(), assign to variable data
    * setUsers to data variable
    */
-
+    async function getUsers() {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setUsers(data);
+      } catch(error) {
+        console.error(error)
+      }
+    }
   /**
    * TODO 3: Set up useEffect with callback and dependency array.
    * MAKE SURE TO INCLUDE DEPENDENCY ARRAY!!!!!!!
    *
    * Invoke getUsers() in useEffect callback
    */
+    useEffect(() => {
+      getUsers()
+    }, [])
 
   return (
     // TODO 4: Set up HTML and iterate over users variable
     // Return list item for each user.
     <>
-      <h2>useEffect - Fetch Data</h2>
+      <ul className='users'>
+        {users.map(({ id, login, avatar_url, html_url }) => (
+            <li key={id}>
+            <img src={avatar_url} alt={login} />
+            <div>
+              <h3>{login}</h3>
+              <a href={html_url} target="_blank" rel="noreferrer">
+                Profile
+              </a>
+            </div>
+          </li>
+          ))}
+      </ul>
     </>
   );
 };
--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
-import ErrorExample from './01-useState/error-example'
-import UseStateBasics from "./01-useState/useState-basics";
-import UseStateArray from './01-useState/useState-array';
-import UseStateObject from './01-useState/useState-object.jsx';
+import UseEffectFetchData from './02-useEffect/useEffect-fetch-data';
+
 function App() {
   return (
     <div className="container">
-      < UseStateObject />
+      < UseEffectFetchData />
     </div>
   )
 }
